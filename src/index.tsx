0ed3b6dd5acb0aead3e3abc8736cbaa7f20662e0
Cassandra Copp <cass16180@gmail.com>
2023-04-20 13:21:34 -0700

    :pencil2: update index file to new tsx format

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import "./index.scss";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.scss';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = createRoot(rootElement);
+root.render(
 	<React.StrictMode>
 		<App />
-	</React.StrictMode>,
-	document.getElementById("root") as HTMLElement
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
