b5121215b3c9324962f347c23614c5144c869c25
Cassandra Copp <cass16180@gmail.com>
2023-04-24 22:20:49 -0700

    :building_construction: properly set up conditional displays of card faces

--- src/Home.tsx
+++ src/Home.tsx
@@ -20,67 +20,82 @@ const buttonStyle = css({
 });
 
 function Home() {
-	const [flip, setFlip] = useState<Boolean>(false);
+	const flipCard = () => {
+		const [flip, setFlip] = useState<String>('home');
+		const handleNewAssignment = () => {
+			setFlip('assign');
+		};
 
-	const handleNewAssignment = () => {
-		setFlip(!flip);
-	};
+		const handleAddYourOwn = () => {
+			setFlip('add');
+			console.log('add your own');
+		};
 
-	const handleAddYourOwn = () => {
-		console.log('add your own');
-	};
+		const handleNarrowDown = () => {
+			setFlip('narrow');
+		};
+		switch (flip) {
+			case 'home':
+				return (
+					<CardContent className="home">
+						<Tooltip title="If you have no idea what to do with yourself, get assigned a random activity.">
+							<Button
+								css={buttonStyle}
+								variant="contained"
+								onClick={handleNewAssignment}
+							>
+								New Assignment
+							</Button>
+						</Tooltip>
+						<Tooltip title="If you are feeling somewhat decisive, take some time to select preferences to help curate a task that you're interested in">
+							<Button
+								css={buttonStyle}
+								variant="contained"
+								onClick={handleNarrowDown}
+							>
+								Narrow it Down
+							</Button>
+						</Tooltip>
+						<Tooltip title="If you have a lot of ideas and some spare time, add your own quests to the mix.">
+							<Button
+								css={buttonStyle}
+								variant="contained"
+								onClick={handleAddYourOwn}
+							>
+								Add Your Own Quest
+							</Button>
+						</Tooltip>
+					</CardContent>
+				);
 
-	const handleNarrowDown = () => {
-		console.log('select filters');
+			case 'assign':
+				return (
+					<CardContent className="assign">
+						<h2>Your Mission</h2>
+						<h3>Organize your closet</h3>
+						<p>
+							- select some items that you don't wear anymore to
+							donate
+						</p>
+					</CardContent>
+				);
+
+			case 'add':
+				return <CardContent className="home">ADD</CardContent>;
+
+			case 'narrow':
+				return <CardContent className="home">filters</CardContent>;
+
+			default:
+				return null;
+		}
 	};
 
 	return (
 		<Container css={containerStyle}>
 			<Grid container spacing={2}>
 				<Grid item xs={4}>
-					<Card css={cardStyle}>
-						{!flip ? (
-							<CardContent className="front">
-								<Tooltip title="If you have no idea what to do with yourself, get assigned a random activity.">
-									<Button
-										css={buttonStyle}
-										variant="contained"
-										onClick={handleNewAssignment}
-									>
-										New Assignment
-									</Button>
-								</Tooltip>
-								<Tooltip title="If you are feeling somewhat decisive, take some time to select preferences to help curate a task that you're interested in">
-									<Button
-										css={buttonStyle}
-										variant="contained"
-										onClick={handleNarrowDown}
-									>
-										Narrow it Down
-									</Button>
-								</Tooltip>
-								<Tooltip title="If you have a lot of ideas and some spare time, add your own quests to the mix.">
-									<Button
-										css={buttonStyle}
-										variant="contained"
-										onClick={handleAddYourOwn}
-									>
-										Add Your Own Quest
-									</Button>
-								</Tooltip>
-							</CardContent>
-						) : (
-							<CardContent className="back">
-								<h2>Your Mission</h2>
-								<h3>Organize your closet</h3>
-								<p>
-									{' '}
-									- select some items that you don't wear
-									anymore to donate
-								</p>
-							</CardContent>
-						)}
-					</Card>
+					<Card css={cardStyle}>{flipCard()}</Card>
 				</Grid>
 				<Grid item xs={4}>
 					<Card css={cardStyle}>Card 2</Card>
